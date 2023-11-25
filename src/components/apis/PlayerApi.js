cf31b4bfb76b687f51c41c4bb82d582d92c7ae6d
umatt1 <matt.burkhard@gmail.com>
2023-11-25 12:07:40 -0500

    Implement PlayerAPI and add .idea to .gitignore

--- src/components/apis/PlayerApi.js
+++ src/components/apis/PlayerApi.js
@@ -1,1 +1,54 @@
-import A
+import axios from "axios";
+
+// Api.js
+import axios from 'axios';
+
+class PlayerApi {
+  constructor(baseURL) {
+    this.instance = axios.create({
+      baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+        // You can add other headers here
+      },
+    });
+  }
+
+  async get(endpoint) {
+    try {
+      const response = await this.instance.get(endpoint);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async post(endpoint, data) {
+    try {
+      const response = await this.instance.post(endpoint, data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async put(endpoint, data) {
+    try {
+      const response = await this.instance.put(endpoint, data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async delete(endpoint) {
+    try {
+      const response = await this.instance.delete(endpoint);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default PlayerApi;
