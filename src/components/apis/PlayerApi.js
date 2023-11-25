4b381aa97b5799ad6ddfd57b0eb7fa0e0c00985a
umatt1 <matt.burkhard@gmail.com>
2023-11-25 14:26:00 -0500

    Implement more connection on PlayerApi

--- src/components/apis/PlayerApi.js
+++ src/components/apis/PlayerApi.js
@@ -1,24 +1,25 @@
 // Api.js
 import axios from 'axios';
 
-require('dotenv').config();
-
 const apiUrl = process.env.REACT_APP_BACKEND_URL;
 
 class PlayerApi {
   constructor() {
     this.instance = axios.create({
-      apiUrl,
-      headers: {
-        'Content-Type': 'application/json',
+      baseURL: apiUrl,
+      //timeout: 10000,
+      //headers: {
+        //'Content-Type': 'application/json',
         // You can add other headers here
-      },
+      //},
     });
   }
 
   async get(endpoint) {
     try {
-      const response = await this.instance.get(endpoint);
+        console.log(endpoint);
+      //const response = await this.instance.get(endpoint);
+      const response = await axios.get(process.env.REACT_APP_BACKEND_URL+endpoint);
       return response.data;
     } catch (error) {
       throw error;
