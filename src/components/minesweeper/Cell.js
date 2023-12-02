6f61a5a06d85d7cb0dba67522f1da40a45f5a6b0
umatt1 <matt.burkhard@gmail.com>
2023-12-02 11:18:00 -0600

    Create right click actions for a cell

--- src/components/minesweeper/Cell.js
+++ src/components/minesweeper/Cell.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './Cell.css'
 
-const Cell = ({ value, isRevealed, onClick, surrounding }) => {
+const Cell = ({ value, isRevealed, onClick, surrounding, isFlagged }) => {
+  function handleContextMenu(event) {
+    event.preventDefault(); // Prevent the default context menu
+    
+    console.log('right clicked on a tile');
+    // You can add more logic here if needed
+  }
+
+  function tileValue(isRevealed, surrounding, isFlagged) {
+    // revealed -> return bomb or the surrounding tiles
+    if (isRevealed) {
+      return (value === 1 ? '💣' : surrounding);
+    } else if (isFlagged) {
+      return <img src='/america.png' alt={'flag'}/>;
+    } else {
+      return ' ';
+    }
+  }
   return (
     <div
       className={`cell ${isRevealed ? 'revealed' : 'clickable'}`}
       onClick={onClick}
+      onContextMenu={handleContextMenu}
     >
-      {isRevealed ? (value === 1 ? '💣' : surrounding) : ' '}
+      {tileValue(isRevealed, surrounding, isFlagged)}
     </div>
   );
 };
