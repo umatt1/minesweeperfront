b3d723763ed4791d80adb5af88d6089d637def6a
umatt1 <matt.burkhard@gmail.com>
2023-12-16 17:13:46 -0500

    Stub out a user page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import User from '../src/pages/User'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/User/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.css";
+
+const User = props => {
+
+  return (
+    <div>
+        <p>This is a users page</p>
+    </div>
+  );
+};
+
+export default User;
