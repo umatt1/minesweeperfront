0bfbc74247e81097afa19d49f01b454010537de4
umatt1 <matt.burkhard@gmail.com>
2024-01-25 20:39:43 -0500

    Stubs out a login form and a user page

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MinesweeperPage from './pages/MinesweeperPage';
+import UserPage from './pages/UserPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MinesweeperPage/>
+  },
+  {
+    path: "/user",
+    element: <UserPage/>
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
