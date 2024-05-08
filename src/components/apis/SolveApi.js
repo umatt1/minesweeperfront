3190ce70243c56c52215ed4d4231b52d4d68bb5d
umatt1 <matt.burkhard@gmail.com>
2024-05-08 00:11:28 -0400

    Resolve timezones once and for all, clean up console logs

--- src/components/apis/SolveApi.js
+++ src/components/apis/SolveApi.js
@@ -16,12 +16,25 @@ class SolveApi extends Api {
 
     async getWeeksSolves(user, token) {
       try {
-        const response = await this.get("/solve/"+user, token);
-        return response;
+        const response = await this.get("/solve/" + user, token);
+        const toReturn = [];
+        response.forEach(e => {
+          // Ensure solve.puzzle.date is a valid Date object
+          const [year, month, day] = e.puzzle.date.split('-').map(Number);
+          const solveDate = new Date(year, month - 1, day); // month - 1 because months are 0-indexed
+          // Get the short weekday string
+          //const solveDayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(solveDate);
+          // Assign the short weekday string to solve object
+          //e.puzzle.dayOfWeek = solveDayOfWeek;
+          e.puzzle.date = solveDate;
+          toReturn.push(e);
+        });
+        return toReturn;
       } catch (error) {
         throw error;
       }
     }
+    
 
 
 }
