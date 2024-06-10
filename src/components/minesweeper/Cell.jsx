c3376daeafce6689aa7b10452af02ef7d2c00357
umatt1 <matt.burkhard@gmail.com>
2024-06-10 08:40:39 -0400

    Add more mobile friendly controls

--- src/components/minesweeper/Cell.jsx
+++ src/components/minesweeper/Cell.jsx
@@ -1,27 +1,68 @@
-import React from 'react';
-import './Cell.css'
+import React, { useState } from 'react';
+import './Cell.css';
 
 const Cell = ({ value, isRevealed, onClick, surrounding, isFlagged, onRightClick }) => {
+  const [timer, setTimer] = useState(null);
+
   function handleContextMenu(event) {
     event.preventDefault(); // Prevent the default context menu
-    onRightClick()
+    onRightClick();
+  }
+
+  function handleMouseDown(event) {
+    if (event.button === 2) {
+      // Right-click
+      event.preventDefault();
+      onRightClick();
+    } else {
+      // Left-click
+      setTimer(setTimeout(() => {
+        onRightClick();
+      }, 150));
+    }
+  }
+
+  function handleMouseUp(event) {
+    if (timer) {
+      clearTimeout(timer);
+      setTimer(null);
+    }
+  }
+
+  function handleTouchStart() {
+    setTimer(setTimeout(() => {
+      onRightClick();
+    }, 500));
+  }
+
+  function handleTouchEnd() {
+    if (timer) {
+      clearTimeout(timer);
+      setTimer(null);
+    }
   }
 
   function tileValue(isRevealed, surrounding, isFlagged) {
     // revealed -> return bomb or the surrounding tiles
     if (isRevealed) {
-      return (value === 1 ? '💣' : surrounding);
+      return value === 1 ? '💣' : surrounding;
     } else if (isFlagged) {
       return '🚩';
     } else {
       return ' ';
     }
   }
+
   return (
     <div
       className={`cell ${isRevealed ? 'revealed' : 'clickable'}`}
       onClick={onClick}
       onContextMenu={handleContextMenu}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
     >
       {tileValue(isRevealed, surrounding, isFlagged)}
     </div>
